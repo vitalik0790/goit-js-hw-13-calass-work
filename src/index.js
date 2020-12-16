6cc5e945b2e734115abb97dea3ee9d897829626e
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2020-12-16 20:13:47 +0200

    class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import './styles.scss';
+
+const API_KEY = '19535506-9e76d01e5d644620db638e0a4';
+const URL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ';
